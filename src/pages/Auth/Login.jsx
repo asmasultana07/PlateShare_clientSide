66bf56a668a6e345237c8f5cd0fd72ef1ee6a95c
asmasultana07 <ab.asma1084@gmail.com>
2026-02-05 01:55:02 +0600

    added login resgister

--- src/pages/Auth/Login.jsx
+++ src/pages/Auth/Login.jsx
@@ -1,33 +1,40 @@
 import React from "react";
-import MyContainer from "../../components/MyContainer";
+import { Link } from "react-router";
+import usePageTitle from "../../hook/usePageTitle";
 
 const Login = () => {
+    usePageTitle("Login")
   return (
       <div className="hero bg-base-200 min-h-screen">
+        
           <div className="hero-content flex-col lg:flex-row ">
             <div className="text-center lg:text-left">
-              <h1 className="text-5xl font-bold">Welcome Back to PlateShare</h1>
-              <p className="py-6 text-lg text-pp">
-                Back to sharing goodness.
+              <h1 className="text-3xl md:text-5xl font-bold">Welcome Back!</h1>
+              <p className="md:py-2 text-lg text-pp">
+                Let’s keep sharing goodness.
               </p>
             </div>
-            <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-              <div className="card-body">
-                <fieldset className="fieldset">
-                  <label className="label font-bold text-lg">Email</label>
-                  <input type="email" className="input" placeholder="Email" />
-                  <label className="label font-bold text-lg">Password</label>
-                  <input
-                    type="password"
-                    className="input"
-                    placeholder="Password"
-                  />
-                  {/* <div>
-                    <a className="link link-hover">Forgot password?</a>
-                  </div> */}
-                  <button className="my-btn">Login</button>
-                </fieldset>
-              </div>
+            <div className="p-4 card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+              
+              <form className="card-body">
+                
+                  <div>
+                    <label className="label font-bold text-lg">Email</label>
+                    <input type="email" className="input input-bordered w-full outline-none" placeholder="Email" />
+                  </div>
+                  <div>
+                    <label className="label font-bold text-lg">Password</label>
+                  <input type="password"  className="input input-bordered w-full outline-none"  placeholder="Password"  />
+                  {/* <div> <a className="link link-hover">Forgot password?</a> </div> */}
+                  </div>
+                  <button type="submit" className="active-btn mt-6">Login</button>
+                    <p className="text-center font-semibold"> or </p>
+                  <button className="my-btn">Continue with Google  </button>
+                  <p>Don't Have an Account? 
+                    <Link to="/registration"><span className="font-semibold text-green-600 "> Register</span></Link>
+                  </p>
+              </form>
+              
             </div>
           </div>
         
--- src/pages/Auth/Register.jsx
+++ src/pages/Auth/Register.jsx
@@ -1,11 +1,46 @@
 import React from "react"
+import { Link } from "react-router";
+import usePageTitle from "../../hook/usePageTitle";
 
 const Register = () => {
 
-
-    return (
-        <div>Register</div>
-    )
+ usePageTitle("Register")
+  return (
+      <div className="hero bg-base-200 min-h-screen">
+         <div className="hero-content flex-col lg:flex-row ">
+            <div className="card bg-base-100 shadow-2xl">              
+              <form className="card-body"> 
+                  <div>
+                    <label className="label font-bold text-lg">Name</label>
+                    <input type="name" className="input input-bordered  outline-none" placeholder="Your Full Name" required />
+                  </div>
+                  <div>
+                    <label className="label font-bold text-lg">Email</label>
+                    <input type="email" className="input input-bordered outline-none" placeholder="Your Email" required/>
+                  </div>
+                  <div>
+                    <label className="label font-bold text-lg">Password</label>
+                  <input type="password"  className="input input-bordered  outline-none"  placeholder="Your Password" required />
+                  </div>
+                  <div>
+                    <label className="label font-bold text-lg">Photo-URL</label>
+                  <input type="photo"  className="input input-bordered  outline-none"  placeholder="Your Photo Url"  required/>
+                  </div>
+                  
+                  <button type="submit" className="active-btn mt-4">Register</button>
+                    <p className="text-center font-semibold"> or </p>
+                  <button className="my-btn">Continue with Google  </button>
+                  <p>Already have an account? 
+                    <Link to="/registration"><span className="font-semibold text-green-600 "> Login</span></Link>
+                  </p>
+              </form>
+              
+            </div>
+          </div>
+        
+      </div>
+ 
+  );
 }
 
 export default Register
